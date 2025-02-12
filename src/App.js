8b6d006638f3c712d2f9133abe2d8b7f9de9fe3f
Agustin Vessoni <agustinvessoni149@gmail.com>
2025-02-11 20:00:03 -0600

    basename fixed

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Certifications from "./Pages/Certifications";
 
 function App() {
   return (
-    <Router basename="/nombre-repositorio">
+    <Router basename="/portfolio">
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
